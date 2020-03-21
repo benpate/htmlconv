bf0cf028340f5f1407516bbde7fad13fdf605734
Ben Pate <ben@pate.org>
2020-03-21 11:06:02 -0600

    Update summary.go
    Now uses only the first paragraph (if possible)

--- summary.go
+++ summary.go
@@ -1,11 +1,21 @@
 package html
 
+import "strings"
+
 // Summary returns the first few sentences of content from an HTML document
 func Summary(html string) string {
 
 	text := ToText(html)
+
+	// If we found a paragraph (or other break, only use the first line)
+	if index := strings.Index(text, "\n"); index > -1 {
+		text = text[:index]
+	}
+
+	// Remove any extra whitespace now.
 	text = CollapseWhitespace(text)
 
+	// If it's STILL too long, then truncate to 200 characters.
 	if len(text) > 200 {
 		text = text[:200] + "..."
 	}
