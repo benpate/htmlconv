98e5f8ba531a3f93157bc31566a9d0c2c9dbdfa0
Ben Pate <ben@pate.org>
2020-03-21 11:14:20 -0600

    Updating FromText
    - adding quotes
    - adding test case
    - clean up file name

--- fromText.go
+++ fromText.go
@@ -9,7 +9,9 @@ func FromText(text string) string {
 
 	text = strings.Replace(text, "<", "&lt;", -1)
 	text = strings.Replace(text, ">", "&gt;", -1)
+	text = strings.Replace(text, `"`, "&quot;", -1)
 	text = strings.Replace(text, "\n", "<br>", -1)
+	text = CollapseWhitespace(text)
 
 	return text
 }
---
+++ fromText_test.go
@@ -0,0 +1,18 @@
+package html
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFromText(t *testing.T) {
+
+	original1 := `This is a less than "<".
+		This is a greater than ">"
+		This is a tag that will get displayed <evil tag="true"/><evil>`
+
+	expected1 := `This is a less than &quot;&lt;&quot;.<br> This is a greater than &quot;&gt;&quot;<br> This is a tag that will get displayed &lt;evil tag=&quot;true&quot;/&gt;&lt;evil&gt;`
+
+	assert.Equal(t, expected1, FromText(original1))
+}
