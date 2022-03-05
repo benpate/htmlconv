59211b2c3d544f3efa5ac2faf90e7bc4980867bb
Ben Pate <ben@pate.org>
2022-03-05 13:53:30 -0700

    Improved summary
    - can now generate correct summaries of plain text, not just HTML

--- summary.go
+++ summary.go
@@ -13,12 +13,18 @@ func init() {
 // Summary returns the first few sentences of content from an HTML document
 func Summary(html string) string {
 
+	// Try to get just the first paragraph of content.
 	result := firstParagraph.FindString(html)
 
-	// Remove any extra whitespace now.
+	// If there is no "paragraph" marker (e.g. this is plain text) then just use the original
+	if result == "" {
+		result = html
+	}
+
+	// Remove all tags and whitespace
 	result = CollapseWhitespace(RemoveTags(result))
 
-	// If it's STILL too long, then truncate to 200 characters.
+	// Truncate to 200 characters (plus ellipsis)
 	if len(result) > 200 {
 		result = result[:200] + "..."
 	}
--- summary_test.go
+++ summary_test.go
@@ -6,11 +6,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSummary(t *testing.T) {
+func TestSummary_PlainText(t *testing.T) {
+	content := "This is the first paragraph of the content.  There is NO HTML."
+	require.Equal(t, "This is the first paragraph of the content. There is NO HTML.", Summary(content))
+}
 
-	content1 := "<p>This is the first paragraph of the content.</p><p>This is the second paragraph of the content</p>"
-	content2 := "<p>This is the first, but really, really long paragraph of the content.  I'm testing to see that we can still truncate to 200 characters even though the first paragraph says otherwise.  I wonder what else I'll have to say in order to make this truncate correctly?</p><p>Here's the second paragraph</p>"
+func TestSummary1(t *testing.T) {
+	content := "<p>This is the first, but really, really long paragraph of the content.  I'm testing to see that we can still truncate to 200 characters even though the first paragraph says otherwise.  I wonder what else I'll have to say in order to make this truncate correctly?</p><p>Here's the second paragraph</p>"
+	require.Equal(t, "This is the first, but really, really long paragraph of the content. I'm testing to see that we can still truncate to 200 characters even though the first paragraph says otherwise. I wonder what else ...", Summary(content))
+}
 
-	require.Equal(t, "This is the first paragraph of the content.", Summary(content1))
-	require.Equal(t, "This is the first, but really, really long paragraph of the content. I'm testing to see that we can still truncate to 200 characters even though the first paragraph says otherwise. I wonder what else ...", Summary(content2))
+func TestSummary2(t *testing.T) {
+	content := "<p>This is the first, but really, really long paragraph of the content.  I'm testing to see that we can still truncate to 200 characters even though the first paragraph says otherwise.  I wonder what else I'll have to say in order to make this truncate correctly?</p><p>Here's the second paragraph</p>"
+	require.Equal(t, "This is the first, but really, really long paragraph of the content. I'm testing to see that we can still truncate to 200 characters even though the first paragraph says otherwise. I wonder what else ...", Summary(content))
 }
